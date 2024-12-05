2cfcaa4c3645c5f8e8d64859f41250fc81d61127
img-gif <Olegmilto15@gmail.com>
2024-12-05 09:49:22 +0300

    Update task2.c

--- 3824Б1ФИ1/19 Мильто Олег Алексеевич/task2.c
+++ 3824Б1ФИ1/19 Мильто Олег Алексеевич/task2.c
@@ -1,6 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  return 0;
+  unsigned long long res = (unsigned long long)a * (unsigned long long)b;
+  if (res >= UINT_MAX)
+    return UINT_MAX;
+  else
+    return 0;
 }
